271a333362a92aedf73aa92b64aca754d1656b1c
Hiro? <brenofprocopio@gmail.com>
2022-08-07 02:55:21 -0300

    v1
    + ui aprimoramentos
    obs: alguns bugs

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -3,10 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
+  slideOpts = {
+    initialSlide: 0,
+    slidesPerView: 5,
+    autoplay: false,
+  };
   constructor() {}
-
 }
